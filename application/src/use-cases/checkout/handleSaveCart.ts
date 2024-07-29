eb0f07051c6d45b82f4e58bc57e945a769ef138b
foxybeatriz <beatrizlukasewicz@gmail.com>
2024-07-29 17:22:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ application/src/use-cases/checkout/handleSaveCart.ts
@@ -0,0 +1,25 @@
+import { ClientInterface } from '@crystallize/js-api-client';
+import { hydrateCart } from '../crystallize/write/editCart';
+
+type Deps = {
+    apiClient: ClientInterface;
+};
+
+export default async (body: any, { apiClient }: Deps, markets?: string[]) => {
+    const cartId = body?.cartId;
+
+    const localCartItems = body?.items?.map((item: any) => ({
+        sku: item.sku,
+        quantity: item.quantity,
+    }));
+
+    try {
+        return await hydrateCart(localCartItems, { apiClient }, cartId, markets);
+    } catch (error: any) {
+        if (error.message.includes('placed')) {
+            console.log('Cart has been placed, creating a new one');
+            return await hydrateCart(localCartItems, { apiClient }, undefined, markets);
+        }
+        throw error;
+    }
+};
